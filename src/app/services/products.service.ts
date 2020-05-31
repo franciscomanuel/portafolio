3bf68bf96bb8df34cb7f38930c543d089affcaea
franciscomanuel <manu0301@gmail.com>
2020-05-31 17:54:20 +0200

    [feature/search]: Implementación del buscador de productos

--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -8,20 +8,41 @@ import {InfoProductInterface} from '../interfaces/info-product-interface';
 export class ProductsService {
 
   products: InfoProductInterface[] = [];
+  productsFilter: InfoProductInterface[] = [];
   loading = true;
 
   constructor(private http: HttpClient) {
-    this.loadProducts();
+    // this.loadProducts();
   }
 
   private loadProducts() {
-    this.http.get('https://angular-html-647e0.firebaseio.com/productos_idx.json').subscribe((product: InfoProductInterface[]) => {
-      this.products = product;
-      this.loading = false;
+    return new Promise((resolve, reject) => {
+      this.http.get('https://angular-html-647e0.firebaseio.com/productos_idx.json').subscribe((product: InfoProductInterface[]) => {
+        this.products = product;
+        this.loading = false;
+        resolve();
+      });
     });
   }
 
   public getProductById(id: string) {
     return this.http.get(`https://angular-html-647e0.firebaseio.com/productos/${id}.json`);
   }
+
+  searchProduct(text: string) {
+    if (this.products.length === 0) {
+      this.loadProducts().then(() => {
+        this.filterProduct(text);
+      });
+    } else {
+      this.filterProduct(text);
+    }
+  }
+
+  private filterProduct(text: string) {
+    text = text.toLowerCase();
+    this.productsFilter = this.products.filter(product => product.categoria.toLowerCase().includes(text)
+      || product.titulo.toLowerCase().includes(text));
+  }
+
 }
